8433e09dfe01c8bbcffdfe3333cb41dd72d846d6
Clément Bauchet <clement.bauchet44@gmail.com>
2018-01-05 17:50:07 +0100

    début config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // définition des points d'entrée
+    // il est possible de définir plusieurs points d'entrée
+    entry: './src/index.js',
+
+    output: {
+        path: "dist",
+        filename: "bundle.js"
+    }
+}
